4dc5cd14834825749c221828e4757c0da7824c44
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2022-10-13 18:40:49 +0300

    small fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,7 @@ document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.burger__menu').classList.toggle('animate');
 });
 
-document
-  .querySelector('.burger__nav-list')
-  .addEventListener('click', () => {
-    document.querySelector('.burger__icon').classList.toggle('active');
-    document.querySelector('.burger__menu').classList.toggle('animate');
-  });
+document.querySelector('.burger__nav-list').addEventListener('click', () => {
+  document.querySelector('.burger__icon').classList.toggle('active');
+  document.querySelector('.burger__menu').classList.toggle('animate');
+});
